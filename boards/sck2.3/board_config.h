916eebd05940b634ad8af1fe687d61c38197be59
oscgonfer <oscgonfer@gmail.com>
2024-09-10 16:07:06 +0200

    Add SCK2.3 bootloader

---
+++ boards/sck2.3/board_config.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Smartcitizen"
+#define PRODUCT_NAME "Smartcitizen Kit 2.3"
+#define VOLUME_LABEL "SCK-23"
+#define INDEX_URL "https://github.com/fablabbcn/smartcitizen-kit-2x/releases/latest"
+#define BOARD_ID "SAMD21G18A-sck2-v3"
+
+#define USB_VID 0x1209
+#define USB_PID 0xCF20
+
+#define LED_PIN PIN_PA19
+#define LED_TX_PIN PIN_PA27
+#define LED_RX_PIN PIN_PA27
+
+#endif
